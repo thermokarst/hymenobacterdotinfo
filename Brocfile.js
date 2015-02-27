ab2866df5b0cb947f08559ec7f4dea274c14c703
Matthew Dillon <mrdillon@alaska.edu>
2015-02-26 20:46:56 -0900

    Refs #1: Simple initial take at progress bar.

--- Brocfile.js
+++ Brocfile.js
@@ -5,5 +5,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 var app = new EmberApp();
 
 app.import('bower_components/flakes/css/all.css');
+app.import('bower_components/nprogress/nprogress.css');
+app.import('bower_components/nprogress/nprogress.js');
 
 module.exports = app.toTree();
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
+/* global NProgress */
 
-export default Ember.Route.extend(ApplicationRouteMixin);
+export default Ember.Route.extend(ApplicationRouteMixin, {
+  actions: {
+    loading: function() {
+      NProgress.start();
+      this.router.one('didTransition', function() {
+        return setTimeout((function() {
+          return NProgress.done();
+        }), 50);
+      });
+      return true;
+    }
+  }
+});
