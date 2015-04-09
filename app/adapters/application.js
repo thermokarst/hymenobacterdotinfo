472eb639bc2eeb5cd5b96692fe61e3488a20cc3b
Matthew Dillon <mrdillon@alaska.edu>
2015-04-09 09:35:32 -0800

    Bake genus into config (prep for clostridium)

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 import config from '../config/environment';
 
 export default DS.RESTAdapter.reopen({
-  namespace: 'api/hymenobacter',
+  namespace: 'api/' + config.genus,
   host: config.apiURL,
   coalesceFindRequests: true,
 });
