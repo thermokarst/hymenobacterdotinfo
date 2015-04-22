b337ca0ee4bd881555607e796f3253429cb76395
Matthew Dillon <mrdillon@alaska.edu>
2015-04-22 11:06:30 -0800

    Oops — extend, not reopen.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 import config from '../config/environment';
 
-export default DS.RESTAdapter.reopen({
+export default DS.RESTAdapter.extend({
   namespace: 'api/' + config.genus,
   host: config.apiURL,
   coalesceFindRequests: true,
--- app/adapters/user.js
+++ app/adapters/user.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 import config from '../config/environment';
 
-export default DS.RESTAdapter.reopen({
+export default DS.RESTAdapter.extend({
   namespace: 'api',
   host: config.apiURL,
   coalesceFindRequests: true,
