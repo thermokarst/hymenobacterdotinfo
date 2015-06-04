33f783bc42943833c4b545e6c1625f9472fdbf3c
Matthew Dillon <mrdillon@alaska.edu>
2015-06-04 10:33:08 -0800

    Add genus from config

--- app/models/species.js
+++ app/models/species.js
@@ -1,10 +1,11 @@
 import DS from 'ember-data';
+import config from '../config/environment';
 
 export default DS.Model.extend({
   speciesName: DS.attr('string'),
   typeSpecies: DS.attr('boolean'),
   etymology: DS.attr('string'),
-  genusName: DS.attr('string'),
+  genusName: DS.attr('string', { defaultValue: config.genus }),
   strains: DS.hasMany('strain', { async: true }),
   totalStrains: DS.attr('number'),
   createdAt: DS.attr('date'),
