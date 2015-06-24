f95b6e4c5f57186b8392cd6d6f2a809e30159496
Matthew Dillon <mrdillon@alaska.edu>
2015-06-23 18:55:16 -0800

    Clean up compare route on exit

--- app/pods/compare/route.js
+++ app/pods/compare/route.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend(AuthenticatedRouteMixin);
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  resetController: function(controller, isExiting /*, transition*/) {
+    if (isExiting) {
+      controller.set('data', null);
+      controller.set('strains', null);
+      controller.set('dataEmpty', true);
+    }
+  }
+});
