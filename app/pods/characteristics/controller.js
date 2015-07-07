963b3591a4c47530013971e0893030bc196fadef
Matthew Dillon <mrdillon@alaska.edu>
2015-07-07 14:47:06 -0800

    characteristics sort order

--- app/pods/characteristics/controller.js
+++ app/pods/characteristics/controller.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  sortParams: ['characteristicType.characteristicTypeName', 'characteristicName'],
+  sortParams: ['characteristicType.characteristicTypeName', 'sortOrder'],
   sortedCharacteristics: Ember.computed.sort('characteristics', 'sortParams'),
 });
