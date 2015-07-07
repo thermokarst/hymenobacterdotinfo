ae7f3bdafaa01c5492ee51fc22729d397d531b67
Matthew Dillon <mrdillon@alaska.edu>
2015-07-07 09:56:49 -0800

    Test-drive metadata

--- app/pods/components/add-button/component.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-import userCanAdd from '../../../utils/user-can-add';
-
-export default Ember.Component.extend({
-  canAdd: function() {
-    let user_role = this.get('session.currentUser.role');
-    return userCanAdd(user_role);
-  }.property('session.currentUser.role').readOnly(),
-});
--- app/pods/species/index/controller.js
+++ app/pods/species/index/controller.js
@@ -3,4 +3,9 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   sortParams: ['speciesName', 'strainCount'],
   sortedSpecies: Ember.computed.sort('species', 'sortParams'),
+
+  metaData: function() {
+    return this.store.metadataFor('species');
+  }.property('model.isLoaded').readOnly(),
+
 });
