3436989a89e5aa76d298a4fae97c54e768f9f210
Matthew Dillon <mrdillon@alaska.edu>
2015-07-07 14:28:16 -0800

    Edit species

---
+++ app/pods/species/show/controller.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  userCanEdit: function() {
+    let meta = this.store.metadataFor('species');
+    let id = this.get('model.id');
+
+    if (meta.canEdit.indexOf( +id ) === -1) {
+      return false
+    }
+    return true;
+  }.property('model.isLoaded').readOnly(),
+
+});
