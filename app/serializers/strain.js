99c94e9c7b9c67bc52b76d4a77e60ac8f9271d62
Matthew Dillon <mrdillon@alaska.edu>
2015-07-10 15:28:27 -0800

    Clean up species id in strain serializer

---
+++ app/serializers/strain.js
@@ -0,0 +1,13 @@
+import DS from 'ember-data';
+
+export default DS.RESTSerializer.extend({
+  isNewSerializerAPI: true,
+
+  serializeBelongsTo: function(snapshot, json, relationship) {
+    var key = relationship.key;
+    var belongsTo = snapshot.belongsTo(key);
+    key = this.keyForRelationship ? this.keyForRelationship(key, "belongsTo", "serialize") : key;
+    json[key] = Ember.isNone(belongsTo) ? belongsTo : +belongsTo.record.id;
+  }
+
+});
