60f518d911e87afd348e203c848dcc2ede4619f8
Matthew Dillon <mrdillon@alaska.edu>
2015-07-14 08:02:49 -0800

    Remove duplicate mixins

--- app/pods/protected/species/edit/route.js
+++ app/pods/protected/species/edit/route.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
-import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend(AuthenticatedRouteMixin, {
+export default Ember.Route.extend({
   afterModel: function(species) {
     if (!species.get('canEdit')) {
       this.transitionTo('species.show', species.get('id'));
