24c870353f5ad4cebc21b0a727561f144eca08d4
Matthew Dillon <mrdillon@alaska.edu>
2015-09-03 10:50:01 -0700

    'Select All' in compare.
    Fixes #8

--- app/pods/protected/compare/controller.js
+++ app/pods/protected/compare/controller.js
@@ -9,6 +9,33 @@ export default Ember.Controller.extend({
       };
 
       this.transitionToRoute('protected.compare.results', {queryParams: query});
-    }
+    },
+
+    selectAllStrains: function() {
+      let strains = this.get('strains');
+      let strain_ids = [];
+      strains.forEach((strain) => {
+        strain_ids.push(strain.id);
+      });
+      this.set('selectedStrains', strain_ids.join(","));
+    },
+
+    deselectAllStrains: function() {
+      this.set('selectedStrains', '');
+    },
+
+    selectAllCharacteristics: function() {
+      let chars = this.get('characteristics');
+      let char_ids = [];
+      chars.forEach((char) => {
+        char_ids.push(char.id);
+      });
+      this.set('selectedCharacteristics', char_ids.join(","));
+    },
+
+    deselectAllCharacteristics: function() {
+      this.set('selectedCharacteristics', '');
+    },
+
   }
 });
