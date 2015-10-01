69dbdcaa8941b934b37f33d4e7e312e59ecb76af
Matthew Dillon <mrdillon@alaska.edu>
2015-10-01 06:46:51 -0700

    Adapter error deprecated.

--- app/pods/application/adapter.js
+++ app/pods/application/adapter.js
@@ -12,24 +12,6 @@ export default DS.RESTAdapter.extend({
 
   coalesceFindRequests: true,
 
-  ajaxError: function(jqXHR) {
-    // http://stackoverflow.com/a/24027443
-    var error = this._super(jqXHR);
-    if (jqXHR && jqXHR.status === 422) {
-      var response = Ember.$.parseJSON(jqXHR.responseText),
-        errors = {};
-      if (response.errors !== undefined) {
-        var jsonErrors = response.errors;
-        Ember.EnumerableUtils.forEach(Object.keys(jsonErrors), function(key) {
-          errors[Ember.String.camelize(key)] = jsonErrors[key];
-        });
-      }
-      return new DS.InvalidError(errors);
-    } else {
-      return error;
-    }
-  },
-
   shouldReloadAll: function() {
     return true;
   },
