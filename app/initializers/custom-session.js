45eca0227c160c2b80e18b8c80a52466209fb7c1
Matthew Dillon <mrdillon@alaska.edu>
2015-10-13 10:31:47 -0700

    Tweak custom session

--- app/initializers/custom-session.js
+++ app/initializers/custom-session.js
@@ -1,25 +1,11 @@
-import Session from 'simple-auth/session';
-import parseBase64 from '../utils/parse-base64';
 import Ember from 'ember';
 
-var CustomSession = Session.extend({
-  currentUser: function() {
-    let token = this.get('secure.token');
-    if (!Ember.isEmpty(token)) {
-      let t = parseBase64(token);
-      return this.container.lookup('service:store').find('user', t['sub']);
-    }
-    return null;
-  }.property('secure.token'),
-
-});
-
 export function initialize(container, application) {
-  application.register('session:custom', CustomSession);
+  application.inject('session:custom', '_store', 'service:store');
 }
 
 export default {
   name: 'custom-session',
-  before: 'simple-auth',
+  after: 'ember-data',
   initialize: initialize
 };
