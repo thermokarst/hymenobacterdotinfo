1de25effb52ec4d491d7535d49c6bbb6d4df4eec
Matthew Dillon <mrdillon@alaska.edu>
2015-10-13 08:47:22 -0700

    Keep non-admins out of user profiles
    Fixes #16.

--- app/pods/protected/users/show/route.js
+++ app/pods/protected/users/show/route.js
@@ -1,6 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  beforeModel: function(transition) {
+    this._super(transition);
+
+    if (this.get('session.currentUser.role') !== 'A') {
+      let user_id = transition.params['protected.users.show'].user_id;
+      let currentUser_id = this.get('session.currentUser.id')
+      if (currentUser_id !== user_id) {
+        this.transitionTo('protected.users.show', currentUser_id);
+      }
+    }
+  },
+
   model: function(params) {
     return this.store.findRecord('user', params.user_id, { reload: true });
   },
