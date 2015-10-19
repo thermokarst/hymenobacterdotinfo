93bdc594f5f834f5e042382fbbb74973f93731b7
Matthew Dillon <matthewrdillon@gmail.com>
2015-10-19 15:22:21 -0700

    Manually sort strains/chars
    Fixes #11.

--- app/pods/protected/strains/show/measurements-table/component.js
+++ app/pods/protected/strains/show/measurements-table/component.js
@@ -12,6 +12,8 @@ export default Ember.Component.extend({
   }.on('didInsertElement'),
 
   sortParams: ['characteristic.characteristicTypeName', 'characteristic.sortOrder', 'characteristic.characteristicName'],
+  sortAsc: true,
+  paramsChanged: false,
   sortedMeasurements: Ember.computed.sort('model.measurements', 'sortParams'),
 
   actions: {
@@ -23,6 +25,22 @@ export default Ember.Component.extend({
         characteristic: c
       });
       this.get('model.measurements').addObject(m);
+    },
+
+    changeSortParam: function(col) {
+      let sort = this.get('sortAsc') ? 'asc' : 'desc';
+      let sortCol = `${col}:${sort}`;
+      this.set('sortParams', [sortCol]);
+      this.set('paramsChanged', true);
+      this.toggleProperty('sortAsc');
+      return false;
+    },
+
+    resetSortParam: function() {
+      this.set('sortParams', ['characteristic.characteristicTypeName', 'characteristic.sortOrder', 'characteristic.characteristicName']);
+      this.set('paramsChanged', false);
+      this.set('sortAsc', true);
+      return false;
     },
   },
 
