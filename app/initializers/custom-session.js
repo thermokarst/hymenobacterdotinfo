1aa8d8d88d14f26eb6c8e6e7a810200e09f8475b
Matthew Dillon <matthewrdillon@gmail.com>
2015-10-20 10:40:23 -0700

    Clear out old stuff

--- app/initializers/custom-session.js
+++
@@ -1,9 +0,0 @@
-export function initialize(container, application) {
-  application.inject('session:custom', '_store', 'service:store');
-}
-
-export default {
-  name: 'custom-session',
-  after: 'ember-data',
-  initialize: initialize
-};
