8e11942521381cdb4c7ac1e5211eee2a8cf40ae6
Matthew Dillon <mrdillon@alaska.edu>
2015-10-21 07:27:33 -0700

    Revert #22

--- app/pods/login/controller.js
+++ app/pods/login/controller.js
@@ -3,20 +3,19 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
 
-  loading: false,
-
   actions: {
     authenticate: function() {
       // Manually clean up because there might not be a transition
       this.get('flashMessages').clearMessages();
+
       let { identification, password } = this.getProperties('identification', 'password');
-      this.set('loading', true);
-      this.get('session').authenticate('authenticator:oauth2', identification, password).catch((error) => {
-        this.transitionToRoute('login');
-        this.set('loading', false);
-        this.get('flashMessages').error(error.error);
+      this.transitionToRoute('loading').then(() => {
+        this.get('session').authenticate('authenticator:oauth2', identification, password).catch((error) => {
+          this.transitionToRoute('login').then(() => {
+            this.get('flashMessages').error(error.error);
+          });
+        });
       });
-      this.set('loading', false);
     }
   }
 });
