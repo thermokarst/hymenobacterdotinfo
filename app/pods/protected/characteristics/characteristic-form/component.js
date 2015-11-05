71066965332c5bfef80db7e9e13946641a935b82
Matthew Dillon <mrdillon@alaska.edu>
2015-11-05 14:05:06 -0700

    Refactor characteristics/edit

--- app/pods/protected/characteristics/characteristic-form/component.js
+++ app/pods/protected/characteristics/characteristic-form/component.js
@@ -1,13 +1,60 @@
 import Ember from 'ember';
+import SetupMetaData from '../../../../mixins/setup-metadata';
 
-export default Ember.Component.extend({
+const { Component } = Ember;
+
+export default Component.extend(SetupMetaData, {
+  // Read-only attributes
+  characteristic: null,
+  isDirty: false,
+
+  // Actions
+  "on-save": null,
+  "on-cancel": null,
+  "on-update": null,
+
+  // Property mapping
+  propertiesList: ['characteristicName', 'characteristicTypeName', 'sortOrder'],
+  characteristicName: null,
+  characteristicTypeName: null,
+  sortOrder: null,
+
+  resetOnInit: Ember.on('init', function() {
+    this.get('propertiesList').forEach((field) => {
+      const valueInCharacteristic = this.get('characteristic').get(field);
+      this.set(field, valueInCharacteristic);
+    });
+  }),
+
+  updateField: function(property, value) {
+    this.set(property, value);
+    // Manually compare against passed in value
+    if (this.get('characteristic').get(property) !== value) {
+      this.set('isDirty', true);
+    } else {
+      this.set('isDirty', false);
+    }
+  },
+
   actions: {
     save: function() {
-      this.sendAction('save');
+      return this.attrs['on-save'](this.getProperties(this.get('propertiesList')));
     },
 
     cancel: function() {
-      this.sendAction('cancel');
+      return this.attrs['on-cancel']();
     },
-  }
+
+    characteristicNameDidChange: function(value) {
+      this.updateField('characteristicName', value);
+    },
+
+    characteristicTypeNameDidChange: function(value) {
+      this.updateField('characteristicTypeName', value);
+    },
+
+    sortOrderDidChange: function(value) {
+      this.updateField('sortOrder', value);
+    },
+  },
 });
