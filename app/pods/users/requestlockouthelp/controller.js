079b2c59f61d8d048b15b9390e0ea8c0a2e63ae3
Matthew Dillon <mrdillon@alaska.edu>
2015-11-10 09:22:35 -0700

    Refactor user lockout/pw reset

--- app/pods/users/requestlockouthelp/controller.js
+++ app/pods/users/requestlockouthelp/controller.js
@@ -1,15 +1,18 @@
 import Ember from 'ember';
 import ajaxRequest from '../../../utils/ajax-request';
 
-export default Ember.Controller.extend({
-  session: Ember.inject.service('session'),
+const { Controller, inject: { service } } = Ember;
+
+export default Controller.extend({
+  session: service(),
+  globals: service(),
 
   actions: {
-    save: function() {
-      let url = `${this.get('globals.apiURL')}/api/${this.get('globals.genus')}/users/lockout`;
-      let options = {
+    submit: function(email) {
+      const url = `${this.get('globals.apiURL')}/api/${this.get('globals.genus')}/users/lockout`;
+      const options = {
         method: 'POST',
-        data: { email: this.get('email') },
+        data: { email: email },
       };
       ajaxRequest(url, options, this.get('session'));
       this.transitionToRoute('login');
