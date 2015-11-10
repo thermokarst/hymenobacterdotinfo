367341c7803ebc5c4c6ca1817416a0685bdc3e35
Matthew Dillon <mrdillon@alaska.edu>
2015-11-10 07:55:08 -0700

    Lint user lockout

--- app/pods/users/lockoutauthenticate/route.js
+++ app/pods/users/lockoutauthenticate/route.js
@@ -1,14 +1,16 @@
 import Ember from 'ember';
 import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
 
-export default Ember.Route.extend(UnauthenticatedRouteMixin, {
-  session: Ember.inject.service('session'),
-  currentUser: Ember.inject.service('session-account'),
+const { Route, get, inject: { service } } = Ember;
+
+export default Route.extend(UnauthenticatedRouteMixin, {
+  session: service(),
+  currentUser: service('session-account'),
 
   beforeModel: function(transition) {
     this._super(transition);
 
-    let token = Ember.get(transition, 'queryParams.token');
+    const token = get(transition, 'queryParams.token');
     this.get('session').authenticate('authenticator:jwt-resolved', token).then(() => {
       this.get('currentUser.account').then((account) => {
         this.transitionTo('protected.users.changepassword', account.get('id'));
