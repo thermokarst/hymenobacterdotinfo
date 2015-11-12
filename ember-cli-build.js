9ed63ca5ba2020cb92410816eef66c4bb9480799
Matthew Dillon <mrdillon@alaska.edu>
2015-11-12 15:43:35 -0700

    deps

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -14,6 +14,8 @@ module.exports = function(defaults) {
   // quill
   app.import('bower_components/quill/dist/quill.base.css');
   app.import('bower_components/quill/dist/quill.snow.css');
+  // selectize
+  app.import('bower_components/selectize/dist/css/selectize.default.css');
 
   // LIBS ////////////////////////////////////////////////////////////////////////
   // flakes (and deps)
@@ -25,6 +27,10 @@ module.exports = function(defaults) {
   app.import('bower_components/moment/moment.js');
   // quill
   app.import('bower_components/quill/dist/quill.min.js');
+  // jquery ui sortable
+  app.import('bower_components/jQuery UI Sortable/jquery-ui-sortable.min.js');
+  // selectize
+  app.import('bower_components/selectize/dist/js/standalone/selectize.min.js');
 
   return app.toTree();
 };
