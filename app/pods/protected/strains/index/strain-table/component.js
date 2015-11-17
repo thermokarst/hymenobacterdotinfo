9d57e1bbfbdf8958e9a7f73ca9b1948ce4911d30
Matthew Dillon <mrdillon@alaska.edu>
2015-11-17 13:16:00 -0700

    Fix up full name, again

--- app/pods/protected/strains/index/strain-table/component.js
+++ app/pods/protected/strains/index/strain-table/component.js
@@ -6,7 +6,7 @@ const { Component, computed: { sort } } = Ember;
 export default Component.extend(SetupMetaData, {
   strains: null,
 
-  sortParams: ['fullName'],
+  sortParams: ['sortOrder'],
   sortedStrains: sort('strains', 'sortParams'),
 
 });
